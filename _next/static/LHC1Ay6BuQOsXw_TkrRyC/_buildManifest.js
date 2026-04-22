287cf2245aee181474bfa92d6ddfa0646077fef5
jonathan-jimenez <jonathanint4life@gmail.com>
2026-04-21 19:11:58 -0500

    fix_for_ghpages

--- _next/static/LHC1Ay6BuQOsXw_TkrRyC/_buildManifest.js
+++ _next/static/LHC1Ay6BuQOsXw_TkrRyC/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/WebSiteNestJS//_next/:path+",
+        "destination": "/WebSiteNestJS/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
